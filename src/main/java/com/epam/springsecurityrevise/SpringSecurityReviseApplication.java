65b707a8bcd6540c2d673ca9276365b47a8e4f25
Olmosbek510 <olmosbekorazboyev@gmail.com>
2025-01-19 17:06:56 +0500

    first commit

---
+++ src/main/java/com/epam/springsecurityrevise/SpringSecurityReviseApplication.java
@@ -0,0 +1,12 @@
+package com.epam.springsecurityrevise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReviseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityReviseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/epam/integration_testing/springsecurityrevise/SpringSecurityReviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.integration_testing.springsecurityrevise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReviseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
